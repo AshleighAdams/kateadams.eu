0209101a1e6a46d4e98b51a828c95fcb43d6abbc
Kate Adams <self@kateadams.eu>
2014-08-31 06:19:49 +0100

    stuffs

--- lua/template.lua
+++ lua/template.lua
@@ -4,11 +4,18 @@ local function escape_section(sect)
 	return sect:lower()
 end
 
+local google_analytics = [[(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-54122256-1', 'auto');
+ga('require', 'displayfeatures');
+ga('require', 'linkid', 'linkid.js');
+ga('send', 'pageview');]]
+
 return {
 	make = function(self, res, content, body)
 		content = content or {}
-		
-		res:append("<!DOCTYPE html>\n") -- tell them HTML is incoming
 		
 		local template = tags.html
 		{
@@ -20,6 +27,7 @@ return {
 				tags.script { src = "/script.js" },
 				tags.link { href = "//fonts.googleapis.com/css?family=Open+Sans:300,600", rel = "stylesheet", type="text/css" },
 				tags.link { href = "/style.css", rel = "stylesheet", type="text/css" },
+				tags.script { google_analytics },
 				unpack(body or {})
 			},
 			tags.body { class = "slide" }
